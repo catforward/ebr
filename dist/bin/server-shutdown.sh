d80bbb9f0fec7d131e75a0191e297067a7c78503
catforward <catforward.gl@gmail.com>
2021-10-25 15:59:44 +0900

    mod<script>: 修改脚本
    1. 伴随jdk升级到17 添加一个答应异常信息的flag
    2. 添加开始结束的时间戳

--- dist/bin/server-shutdown.sh
+++ dist/bin/server-shutdown.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+SCRIPT_ROOT=$(cd $(dirname $0); pwd)
+export EBR_ROOT=$(cd ${SCRIPT_ROOT}/..; pwd)
 APP_NAME=ebr-server.jar
 
+CLI_LOG=${EBR_ROOT}/logs/${APP_NAME}.log
+
 is_exist() {
-	pid=$(ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}')
-	if [ -z ${pid} ]; then
-		return 1
-	else
-		return 0
-	fi
+    pid=$(ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}')
+    if [ -z ${pid} ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 is_exist
 if [ $? -eq 0 ]; then
-	echo "${APP_NAME} is already running. pid=${pid} shutdown...."
-  kill -15 ${pid}
+    echo "${APP_NAME} is already running. pid=${pid} shutdown...."
+    END_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "--------------------- END  : ${END_TIME} ---------------------" >> ${CLI_LOG} 2>&1
+    kill -15 ${pid}
 else
-  echo "${APP_NAME} is not running."
+    echo "${APP_NAME} is not running."
 fi
