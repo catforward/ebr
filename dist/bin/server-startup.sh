6a55533bd62208bf4f80ab5bfde2958c2340594e
catforward <catforward.gl@gmail.com>
2021-10-25 15:59:44 +0900

    mod<script>: 修改脚本
    1. 伴随jdk升级到17 添加一个答应异常信息的flag
    2. 添加开始结束的时间戳

--- dist/bin/server-startup.sh
+++ dist/bin/server-startup.sh
@@ -7,7 +7,7 @@ APP_JAR=${EBR_ROOT}/libs/${APP_NAME}
 CLI_LOG=${EBR_ROOT}/logs/${APP_NAME}.log
 
 JAVA_BIN=/usr/bin/java
-JAVA_OPTS="-Djava.awt.headless=true -Xms32m -Xmx64m -XX:+UseG1GC -Xlog:gc:${EBR_ROOT}/logs/gc_${APP_NAME}.log"
+JAVA_OPTS="-Djava.awt.headless=true -Xms32m -Xmx64m -XX:+ShowCodeDetailsInExceptionMessages -XX:+UseG1GC -Xlog:gc:${EBR_ROOT}/logs/gc_${APP_NAME}.log"
 
 is_exist() {
     pid=$(ps -ef | grep ${APP_NAME} | grep -v grep | awk '{print $2}')
@@ -25,6 +25,6 @@ if [ $? -eq 0 ]; then
     echo "${APP_NAME} is already running. pid=${pid}"
 else
     START_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "--------------------- ${START_TIME} ---------------------" >> ${CLI_LOG} 2>&1
+    echo "--------------------- START: ${START_TIME} ---------------------" >> ${CLI_LOG} 2>&1
     nohup ${JAVA_BIN} ${JAVA_OPTS} -jar ${APP_JAR} $@ >/dev/null 2>&1 &
 fi
